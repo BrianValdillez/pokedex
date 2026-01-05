79ed7b32e1df21336e89e44be77b4359c4a7217d
Brian Valdillez <valdillez+codebending@alumni.usc.edu>
2026-01-04 17:22:11 -0800

    REPL Implementation

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,27 @@
+import { createInterface } from 'node:readline/promises';
+import { stdin, stdout } from 'node:process';
+
+export function startREPL(){ 
+    const rl = createInterface({ 
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+     });
+
+    rl.on('line', (line:string) => {
+        const args = cleanInput(line);
+        if (args.length == 0){
+            rl.prompt();
+            return;
+        }
+
+        console.log(`Your command was: ${args[0]}`);
+        rl.prompt();
+     });
+
+     rl.prompt();
+}
+
 export function cleanInput(input: string): string[] {
     return input.trim().toLowerCase().split(/\s+/);
 }
